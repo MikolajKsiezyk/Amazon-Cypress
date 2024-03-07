073cdbc248d93653917432e79a9febdbd3bfd21f
Mikolaj K <97797456+ttls69@users.noreply.github.com>
2024-03-07 21:15:10 +0100

    fix browsing history test

--- cypress/e2e/browsingHistory/browsing-history.js
+++ cypress/e2e/browsingHistory/browsing-history.js
@@ -16,5 +16,7 @@ export function validateItemHistory(searchVal) {
     selectProduct().then((productTitle) => {
         Navigate.browsingHistory.here()
         cy.get('div.p13n-sc-truncate-fallback').eq(0).should('have.text',productTitle)
+        cy.reload()
+        cy.wait(10000)
     })
 }
