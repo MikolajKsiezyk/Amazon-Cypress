207c59a3de76c160ccec1b0ffb76be34de06c97c
Mikolaj K <97797456+ttls69@users.noreply.github.com>
2024-03-07 21:07:35 +0100

    Add browsing history tests

--- cypress/e2e/browsingHistory/browsing-history.cy.js
+++ cypress/e2e/browsingHistory/browsing-history.cy.js
@@ -2,13 +2,13 @@ import {clearHistory, validateItemHistory} from "./browsing-history";
 import {restoreCookies} from "../../fixtures/amazonUtils";
 
 
-describe('Home Page tests', () => {
+describe('Browsing History tests', () => {
     beforeEach(()=>{
         restoreCookies()
         clearHistory()
     })
 
-    it('Should validate user logged user info in Home Page', () => {
+    it('Should find product, open it and then check if it is showed in browsing history tab', () => {
         validateItemHistory('Umbrella')
     })
 })
